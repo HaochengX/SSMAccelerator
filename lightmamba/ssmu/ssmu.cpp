b87234d2e1f719187d152ccfc3b1f18ec6f15fd7
Leo Han <147229636+leohan1@users.noreply.github.com>
2025-10-01 09:28:36 +0800

    Update ssmu.cpp

--- lightmamba/ssmu/ssmu.cpp
+++ lightmamba/ssmu/ssmu.cpp
@@ -44,39 +44,49 @@ void exp1(DTYPE_VEC in[VEC_N], DTYPE_VEC out[VEC_N]) {
 
 //1 dimension conv layer
 void conv1d_vec(DTYPE_VEC input_X[VEC_N], DTYPE kernel[K], DTYPE_VEC Y[VEC_N]) {
-    DTYPE input_scalar[INPUT_DIM];
-    unpack_input: for(int i = 0; i < VEC_N; i++) {
-        for(int k = 0; k < VEC_FACTOR; k++) {
-            #pragma HLS PIPELINE II=1
-            int idx = i * VEC_FACTOR + k;
-            if(idx < INPUT_DIM) {
-                input_scalar[idx] = input_X[i][k];
-            }
-        }
+    #pragma HLS INLINE OFF
+    
+    // 移位寄存器，大小 = K + VEC_FACTOR - 1
+    static DTYPE shift_reg[K + VEC_FACTOR - 1];
+    #pragma HLS ARRAY_PARTITION variable=shift_reg complete
+    
+    // 初始化移位寄存器
+    init_shift_reg: for(int i = 0; i < K + VEC_FACTOR - 1; i++) {
+        #pragma HLS UNROLL
+        shift_reg[i] = 0;
     }
-    DTYPE Y_scalar[N];
-    conv_outer: for (int i = 0; i < N; i++) {
+    
+    process_vectors: for(int i = 0; i < VEC_N; i++) {
         #pragma HLS PIPELINE II=1
-        DTYPE sum = 0;
-        conv_inner: for (int j = 0; j < K; j++) {
+        
+        DTYPE_VEC input_vec = input_X[i];
+        DTYPE_VEC output_vec;
+        
+        // 更新移位寄存器
+        update_shift_reg: for(int j = K + VEC_FACTOR - 2; j >= VEC_FACTOR; j--) {
             #pragma HLS UNROLL
-            sum += kernel[j] * input_scalar[i+j];
+            shift_reg[j] = shift_reg[j - VEC_FACTOR];
         }
-        Y_scalar[i] = sum;
-    }
-    pack_output: for(int i = 0; i < VEC_N; i++) {
-        #pragma HLS PIPELINE II=1
-        DTYPE_VEC tmp;
-        for(int k = 0; k < VEC_FACTOR; k++) {
+        
+        // 加载新数据
+        load_data: for(int j = 0; j < VEC_FACTOR; j++) {
             #pragma HLS UNROLL
-            int idx = i * VEC_FACTOR + k;
-            if(idx < N) {
-                tmp[k] = Y_scalar[idx];
-            } else {
-                tmp[k] = 0;
+            shift_reg[j] = input_vec[j];
+        }
+        
+        // 计算卷积
+        compute_output: for(int j = 0; j < VEC_FACTOR; j++) {
+            #pragma HLS UNROLL
+            
+            DTYPE sum = 0;
+            conv_calc: for(int k = 0; k < K; k++) {
+                #pragma HLS UNROLL
+                sum += kernel[k] * shift_reg[j + k];
             }
+            output_vec[j] = sum;
         }
-        Y[i] = tmp;
+        
+        Y[i] = output_vec;
     }
 }
 
@@ -95,15 +105,96 @@ void EAU(DTYPE_VEC A[VEC_N], DTYPE_VEC B[VEC_N], DTYPE_VEC out[VEC_N]) {
 }
 
 //accumulator
-void accumulate(DTYPE_VEC accumulator[VEC_N], DTYPE_VEC addend[VEC_N]) {
-    accumulate_loop: for(int j = 0; j < VEC_N; j++) {
-        accumulator[j] = accumulator[j] + addend[j];
+// void accumulate(DTYPE_VEC accumulator[VEC_N], DTYPE_VEC addend[VEC_N]) {
+//     accumulate_loop: for(int j = 0; j < VEC_N; j++) {
+//         accumulator[j] = accumulator[j] + addend[j];
+//     }
+// }
+
+void producer_function(
+    DTYPE_VEC ddA[VEC_N], DTYPE_VEC ddX[VEC_N], DTYPE_VEC ddB[VEC_N], DTYPE_VEC dC[VEC_N],
+    DTYPE_VEC yy0[VEC_N], DTYPE_VEC H0[M][VEC_N],
+    hls::stream<DTYPE_VEC> &stream_ddA,
+    hls::stream<DTYPE_VEC> &stream_ddX,
+    hls::stream<DTYPE_VEC> &stream_ddB,
+    hls::stream<DTYPE_VEC> &stream_dC,
+    hls::stream<DTYPE_VEC> &stream_yy0,
+    hls::stream<DTYPE_VEC> &stream_H0_in) {
+    
+    write_const_streams: for(int j = 0; j < VEC_N; j++) {
+        #pragma HLS PIPELINE II=1
+        stream_ddA.write(ddA[j]);
+        stream_ddX.write(ddX[j]); 
+        stream_ddB.write(ddB[j]);
+        stream_dC.write(dC[j]);
+        stream_yy0.write(yy0[j]);
+    }
+    
+    write_H0_stream: for(int i = 0; i < M; i++) {
+        for(int j = 0; j < VEC_N; j++) {
+            #pragma HLS PIPELINE II=1
+            stream_H0_in.write(H0[i][j]);
+        }
     }
 }
 
-//packed transfer
-//vector
-//stream
+void consumer_function(
+    hls::stream<DTYPE_VEC> &stream_ddA,
+    hls::stream<DTYPE_VEC> &stream_ddX,
+    hls::stream<DTYPE_VEC> &stream_ddB,
+    hls::stream<DTYPE_VEC> &stream_dC,
+    hls::stream<DTYPE_VEC> &stream_yy0,
+    hls::stream<DTYPE_VEC> &stream_H0_in,
+    DTYPE_VEC H1[M][VEC_N],
+    DTYPE_VEC yy0[VEC_N]) {
+    
+    DTYPE_VEC yy0_accum[VEC_N];
+    
+    init_accum: for(int j = 0; j < VEC_N; j++) {
+        #pragma HLS PIPELINE II=1
+        yy0_accum[j] = stream_yy0.read();
+    }
+
+    process_timesteps: for(int i = 0; i < M; i++) {
+        #pragma HLS PIPELINE II=1
+        
+        DTYPE_VEC H0_current[VEC_N];
+        read_H0_current: for(int j = 0; j < VEC_N; j++) {
+            #pragma HLS PIPELINE II=1
+            H0_current[j] = stream_H0_in.read();
+        }
+        
+        process_elements: for(int j = 0; j < VEC_N; j++) {
+            #pragma HLS PIPELINE II=1
+            
+            DTYPE_VEC ddA_val = stream_ddA.read();
+            DTYPE_VEC ddX_val = stream_ddX.read();
+            DTYPE_VEC ddB_val = stream_ddB.read();
+            DTYPE_VEC dC_val = stream_dC.read();
+            
+            DTYPE_VEC hh0 = H0_current[j] * ddA_val;
+            DTYPE_VEC hh1 = ddX_val * ddB_val;
+            DTYPE_VEC H1_val = hh0 + hh1;
+            
+            H1[i][j] = H1_val;
+            
+            DTYPE_VEC dH_val = dC_val * H1_val;
+            yy0_accum[j] = yy0_accum[j] + dH_val;
+            
+            if (i < M - 1) {
+                stream_ddA.write(ddA_val);
+                stream_ddX.write(ddX_val);
+                stream_ddB.write(ddB_val);
+                stream_dC.write(dC_val);
+            }
+        }
+    }
+
+    write_final_yy0: for(int j = 0; j < VEC_N; j++) {
+        #pragma HLS PIPELINE II=1
+        yy0[j] = yy0_accum[j];
+    }
+}
 
 //top function
 void SSMU(
@@ -130,19 +221,20 @@ void SSMU(
 #pragma HLS INTERFACE s_axilite register port=return bundle=ctrl
 
     hls::stream<DTYPE_VEC> stream_ddA;
-    hls::stream<DTYPE_VEC> stream_ddX;
+    hls::stream<DTYPE_VEC> stream_ddX; 
     hls::stream<DTYPE_VEC> stream_ddB;
     hls::stream<DTYPE_VEC> stream_dC;
-    hls::stream<DTYPE_VEC> stream_H0[M];
-    hls::stream<DTYPE_VEC> stream_H1[M];
-    hls::stream<DTYPE_VEC> stream_dH[M];
+    hls::stream<DTYPE_VEC> stream_H0_in;
+    hls::stream<DTYPE_VEC> stream_H1_out;
     hls::stream<DTYPE_VEC> stream_yy0;
     
-    #pragma HLS STREAM variable=stream_ddA depth=64
-    #pragma HLS STREAM variable=stream_ddX depth=64
-    #pragma HLS STREAM variable=stream_ddB depth=64
-    #pragma HLS STREAM variable=stream_dC depth=64
-    #pragma HLS STREAM variable=stream_yy0 depth=64
+    #pragma HLS STREAM variable=stream_ddA depth=32
+    #pragma HLS STREAM variable=stream_ddX depth=32
+    #pragma HLS STREAM variable=stream_ddB depth=32
+    #pragma HLS STREAM variable=stream_dC depth=32
+    #pragma HLS STREAM variable=stream_H0_in depth=32
+    #pragma HLS STREAM variable=stream_H1_out depth=32
+    #pragma HLS STREAM variable=stream_yy0 depth=32
 
     DTYPE kernel_buffer[K];
 
@@ -182,31 +274,13 @@ void SSMU(
     EMU(ddX, D, yy1);
     exp1(dA, ddA);
 
-    write_ddA: for(int j = 0; j < VEC_N; j++) {
-        #pragma HLS PIPELINE II=1
-        stream_ddA.write(ddA[j]);
-    }
+    #pragma HLS DATAFLOW
     
-    write_ddX: for(int j = 0; j < VEC_N; j++) {
-        #pragma HLS PIPELINE II=1
-        stream_ddX.write(ddX[j]);
-    }
+producer_function(ddA, ddX, ddB, dC, yy0, H0, 
+                     stream_ddA, stream_ddX, stream_ddB, stream_dC, stream_yy0, stream_H0_in);
     
-    write_ddB: for(int j = 0; j < VEC_N; j++) {
-        #pragma HLS PIPELINE II=1
-        stream_ddB.write(ddB[j]);
-    }
-    
-    write_dC: for(int j = 0; j < VEC_N; j++) {
-        #pragma HLS PIPELINE II=1
-        stream_dC.write(dC[j]);
-    }
-    
-    write_yy0: for(int j = 0; j < VEC_N; j++) {
-        #pragma HLS PIPELINE II=1
-        stream_yy0.write(yy0[j]);
-    }
-
+consumer_function(stream_ddA, stream_ddX, stream_ddB, stream_dC, stream_yy0, stream_H0_in,
+                     H1, yy0);
 //     update_H: for(int i=0; i<M; i++){
 // #pragma HLS PIPELINE
 // DTYPE_VEC hh0[VEC_N], hh1[VEC_N], dH[VEC_N];
@@ -217,45 +291,6 @@ void SSMU(
 //         accumulate(yy0, dH);
         
 //     }
- update_H: for(int i = 0; i < M; i++) {
-        #pragma HLS PIPELINE II=1
-        
-        for(int j = 0; j < VEC_N; j++) {
-            #pragma HLS PIPELINE II=1
-            
-            DTYPE_VEC ddA_val = stream_ddA.read();
-            DTYPE_VEC ddX_val = stream_ddX.read();
-            DTYPE_VEC ddB_val = stream_ddB.read();
-            DTYPE_VEC dC_val = stream_dC.read();
-            DTYPE_VEC yy0_val = stream_yy0.read();
-
-            DTYPE_VEC hh0, hh1, H1_val, dH_val;
-            hh0 = H0[i][j] * ddA_val;
-            hh1 = ddX_val * ddB_val;
-            H1_val = hh0 + hh1;
-            H1[i][j] = H1_val;
-            
-            dH_val = dC_val * H1_val;
-            DTYPE_VEC new_yy0 = yy0_val + dH_val;
-            
-            stream_yy0.write(new_yy0);
-        }
-        
-        if (i < M - 1) {
-            refill_streams: for(int j = 0; j < VEC_N; j++) {
-                #pragma HLS PIPELINE II=1
-                stream_ddA.write(ddA[j]);
-                stream_ddX.write(ddX[j]);
-                stream_ddB.write(ddB[j]);
-                stream_dC.write(dC[j]);
-            }
-        }
-    }
-
-read_final_yy0: for(int j = 0; j < VEC_N; j++) {
-        #pragma HLS PIPELINE II=1
-        yy0[j] = stream_yy0.read();
-    }
 
     EAU(yy0, yy1, Y);
     EMU(Y, dZ, out);
